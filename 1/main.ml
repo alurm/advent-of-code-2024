01ac4bf548cd49c0d7181083ddd35e5fab6e9e4c
Alan Urmancheev <alan.urman@gmail.com>
2024-12-09 16:16:21 +0400

    Refactor day one

--- 1/main.ml
+++ 1/main.ml
@@ -3,29 +3,25 @@ let scan_line h = Scanf.bscanf_opt h "%d %d\n"
 let scan h =
   let open Dynarray in
   let left, right = create(), create() in
-  let rec loop () =
+  let rec loop() =
     match scan_line h @@ fun l r ->
       add_last left l;
       add_last right r;
     with
     | None -> ()
-    | Some () -> loop ()
+    | Some() -> loop()
   in
-  loop ();
+  loop();
   to_array left, to_array right
 
-let main () =
+let main() =
   let open Array in
+  let l, r = In_channel.with_open_bin "input.txt" @@ fun h -> scan @@ Scanf.Scanning.from_channel h in
   let sort = sort compare in
-  let l, r = In_channel.with_open_bin "input.txt" @@ fun h ->
-    let h = Scanf.Scanning.from_channel h in
-    scan h
-  in
   sort l;
   sort r;
   let pairs = combine l r in
-  let diffs = map (fun (x, y) -> Int.abs @@ x - y) pairs in
-  let sum = fold_left (+) 0 diffs in
-  print_int sum
+  let diffs = map(fun (x, y) -> Int.abs(x - y)) pairs in
+  Printf.printf "%d\n" @@ fold_left(+) 0 diffs
 
-let _ = main ()
+let _ = main()
